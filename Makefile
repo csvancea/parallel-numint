857cecd7ba73b8bf55c146592e186ed3a3631dfc
Cosmin Vancea <73666215+csvancea@users.noreply.github.com>
2021-11-30 20:10:46 +0200

    Add OpenMPI+OpenMP combo implementation

--- Makefile
+++ Makefile
@@ -3,23 +3,25 @@ CFLAGS      = -Wall -Wextra -march=native -O0 -Iinclude/
 LDFLAGS     = -lm
 
 OMP_CC      = gcc
-OMP_CFLAGS  = -fopenmp $(CFLAGS)
-OMP_LDFLAGS = -fopenmp $(LDFLAGS)
+OMP_CFLAGS  = -fopenmp
+OMP_LDFLAGS = -fopenmp
 
 MPI_CC      = mpicc
-MPI_CFLAGS  = $(CFLAGS)
-MPI_LDFLAGS = $(LDFLAGS)
+MPI_CFLAGS  =
+MPI_LDFLAGS =
 
 
-all: bin/serial bin/omp bin/mpi
+all: bin/serial bin/omp bin/mpi bin/mpi_omp
 
 # Executables
 bin/serial: bin/obj/serial.o bin/obj/main.o bin/obj/math_funcs.o
 	$(CC) -o $@ $^ $(LDFLAGS)
 bin/omp: bin/obj/omp.o bin/obj/main.o bin/obj/math_funcs.o
-	$(OMP_CC) -o $@ $^ $(OMP_LDFLAGS)
+	$(OMP_CC) -o $@ $^ $(LDFLAGS) $(OMP_LDFLAGS)
 bin/mpi: bin/obj/mpi.o bin/obj/main.o bin/obj/math_funcs.o
-	$(MPI_CC) -o $@ $^ $(MPI_LDFLAGS)
+	$(MPI_CC) -o $@ $^ $(LDFLAGS) $(MPI_LDFLAGS)
+bin/mpi_omp: bin/obj/mpi_omp.o bin/obj/main.o bin/obj/math_funcs.o
+	$(MPI_CC) -o $@ $^ $(LDFLAGS) $(MPI_LDFLAGS) $(OMP_LDFLAGS)
 
 # Serial objs
 bin/obj/serial.o: src/impl/serial.c
@@ -27,11 +29,15 @@ bin/obj/serial.o: src/impl/serial.c
 
 # OpenMP objs
 bin/obj/omp.o: src/impl/omp.c
-	$(OMP_CC) -o $@ -c $^ $(OMP_CFLAGS)
+	$(OMP_CC) -o $@ -c $^ $(CFLAGS) $(OMP_CFLAGS)
 
 # MPI objs
 bin/obj/mpi.o: src/impl/mpi.c
-	$(MPI_CC) -o $@ -c $^ $(MPI_CFLAGS)
+	$(MPI_CC) -o $@ -c $^ $(CFLAGS) $(MPI_CFLAGS)
+
+# MPI + OpenMP objs
+bin/obj/mpi_omp.o: src/impl/mpi_omp.c
+	$(MPI_CC) -o $@ -c $^ $(CFLAGS) $(MPI_CFLAGS) $(OMP_CFLAGS)
 
 # General objs
 bin/obj/main.o: src/main.c
@@ -40,4 +46,4 @@ bin/obj/math_funcs.o: src/math_funcs.c
 	$(CC) -o $@ -c $^ $(CFLAGS)
 
 clean:
-	rm -f bin/serial bin/obj/*.o
+	rm -f bin/serial bin/omp bin/mpi bin/mpi_omp bin/obj/*.o
---
+++ src/impl/mpi_omp.c
@@ -0,0 +1,91 @@
+#include <mpi.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <math.h>
+#include "numint.h"
+
+#define MASTER 0
+#define min(a, b) (a) < (b) ? (a) : (b)
+
+enum {
+    SUM_ODDS = 0,
+    SUM_EVENS,
+    SUM_COUNT
+};
+
+
+static void numint_impl(int rank, int nproc, onedim_func_t f, double a, double h, unsigned n, double *local_sums)
+{
+    /* there are (n - 1) inner points which will be equally spread among the processes. */
+    unsigned start =    ((rank + 0) * (n - 1) / nproc + 1   );
+    unsigned end   = min((rank + 1) * (n - 1) / nproc + 1, n);
+    
+    double sum_odds  = 0.0;
+    double sum_evens = 0.0;
+
+    #pragma omp parallel shared(a, h, n)
+    {
+        /*
+         * i must be an odd integer here.
+         * This bit trickery sets i to the first odd integer greater or equal to start.
+         */
+
+        #pragma omp for reduction(+:sum_odds) nowait
+        for (unsigned i = start | 1U; i < end; i += 2)
+        {
+            sum_odds += f(fma(i, h, a));
+        }
+        
+        /*
+         * i must be an even integer here.
+         * This bit trickery sets i to the first even integer greater or equal to start.
+         */
+
+        #pragma omp for reduction(+:sum_evens)
+        for (unsigned i = (start + 1) & ~1U; i < end; i += 2)
+        {
+            sum_evens += f(fma(i, h, a));
+        }
+    }
+
+    local_sums[SUM_ODDS ] = sum_odds;
+    local_sums[SUM_EVENS] = sum_evens;
+}
+
+double numint(onedim_func_t f, double a, double b, unsigned n)
+{
+    int rank, nproc;
+    double local_sums[SUM_COUNT], global_sums[SUM_COUNT];
+    double h = (b - a) / n;
+    double result = 0.0;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    numint_impl(rank, nproc, f, a, h, n, local_sums);
+    MPI_Reduce(local_sums, global_sums, SUM_COUNT, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+
+    if (rank == MASTER) {
+        /* compute f(a), f(b) and the overall sum/result */
+        result = h / 3 * (fma(2, global_sums[SUM_EVENS], f(a)) + fma(4, global_sums[SUM_ODDS], f(b)));
+    }
+
+    return result;
+}
+
+void numint_init()
+{
+    MPI_Init(NULL, NULL);
+}
+
+void numint_end()
+{
+    MPI_Finalize();
+}
+
+int numint_is_master()
+{
+    int rank;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    return (rank == MASTER);
+}

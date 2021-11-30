8044178477bdc3b42a7c7ee29581fc8c33595e47
Cosmin Vancea <73666215+csvancea@users.noreply.github.com>
2021-11-30 19:56:54 +0200

    Optimization: make only 1 call to `MPI_Reduce`

--- src/impl/mpi.c
+++ src/impl/mpi.c
@@ -6,15 +6,21 @@
 #define MASTER 0
 #define min(a, b) (a) < (b) ? (a) : (b)
 
+enum {
+    SUM_ODDS = 0,
+    SUM_EVENS,
+    SUM_COUNT
+};
 
-static void numint_impl(int rank, int nproc, onedim_func_t f, double a, double h, unsigned n, double *sum_odds, double *sum_evens)
+
+static void numint_impl(int rank, int nproc, onedim_func_t f, double a, double h, unsigned n, double *local_sums)
 {
     /* there are (n - 1) inner points which will be equally spread among the processes. */
     unsigned start =    ((rank + 0) * (n - 1) / nproc + 1   );
     unsigned end   = min((rank + 1) * (n - 1) / nproc + 1, n);
     
-    *sum_odds  = 0.0;
-    *sum_evens = 0.0;
+    double sum_odds  = 0.0;
+    double sum_evens = 0.0;
 
     /*
      * i must be an odd integer here.
@@ -22,7 +28,7 @@ static void numint_impl(int rank, int nproc, onedim_func_t f, double a, double h
      */
     for (unsigned i = start | 1U; i < end; i += 2)
     {
-        *sum_odds += f(fma(i, h, a));
+        sum_odds += f(fma(i, h, a));
     }
     
     /*
@@ -31,29 +37,28 @@ static void numint_impl(int rank, int nproc, onedim_func_t f, double a, double h
      */
     for (unsigned i = (start + 1) & ~1U; i < end; i += 2)
     {
-        *sum_evens += f(fma(i, h, a));
+        sum_evens += f(fma(i, h, a));
     }
+
+    local_sums[SUM_ODDS ] = sum_odds;
+    local_sums[SUM_EVENS] = sum_evens;
 }
 
 double numint(onedim_func_t f, double a, double b, unsigned n)
 {
     int rank, nproc;
-    double local_sum_odds, local_sum_evens;
-    double global_sum_odds, global_sum_evens;
+    double local_sums[SUM_COUNT], global_sums[SUM_COUNT];
     double h = (b - a) / n;
     double result = 0.0;
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
-
-    numint_impl(rank, nproc, f, a, h, n, &local_sum_odds, &local_sum_evens);
-
-    MPI_Reduce(&local_sum_odds, &global_sum_odds, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
-    MPI_Reduce(&local_sum_evens, &global_sum_evens, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+    numint_impl(rank, nproc, f, a, h, n, local_sums);
+    MPI_Reduce(local_sums, global_sums, SUM_COUNT, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
 
     if (rank == MASTER) {
-        /* compute a and b and the overall sum/result */
-        result = h / 3 * (fma(2, global_sum_evens, f(a)) + fma(4, global_sum_odds, f(b)));
+        /* compute f(a), f(b) and the overall sum/result */
+        result = h / 3 * (fma(2, global_sums[SUM_EVENS], f(a)) + fma(4, global_sums[SUM_ODDS], f(b)));
     }
 
     return result;
